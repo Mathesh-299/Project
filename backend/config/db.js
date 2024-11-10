731b7ee0c4d3dcd1332a794ad4c5090035291202
Mathesh M <157198679+Mathesh-299@users.noreply.github.com>
2024-11-11 00:49:45 +0530

    push

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGOURI);
         console.log(`DB connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Connection error: ${error.stack}`);
