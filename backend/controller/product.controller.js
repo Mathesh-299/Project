30b4691cc3f869ad6c331fe1ce153c32c4d855fe
Mathesh M <157198679+Mathesh-299@users.noreply.github.com>
2024-11-12 23:49:45 +0530

    big error

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -12,7 +12,7 @@ export const getProducts = async (req, res) => {
 
 }
 
-export const createProducts =async (req, res) => {
+export const createProducts = async (req, res) => {
     const product = req.body;
     if (!product.name || !product.price || !product.image) {
         return res.status(404).json({ success: false, message: "Please enter the fields" })
@@ -31,19 +31,6 @@ export const createProducts =async (req, res) => {
 }
 
 
-export const deleteProduct = async (req, res) => {
-    const { id } = req.params;
-    console.log("id", id);
-    try {
-        await Product.findByIdAndDelete(id);
-        res.status(200).json({ success: true, message: "Product deleted" });
-        // console.log(res.json());S
-    } catch (error) {
-        res.status(500).json({ success: false, message: "Product Not found" });
-
-    }
-}
-
 export const updateProduct = async (req, res) => {
     const { id } = req.params;
     const products = req.body;
@@ -61,3 +48,22 @@ export const updateProduct = async (req, res) => {
     }
 
 }
+
+
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params;
+    console.log("id", id);
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Please enter Valid id" })
+    }
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product deleted" });
+        // console.log(res.json());S
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Product Not found" });
+
+    }
+}
+
